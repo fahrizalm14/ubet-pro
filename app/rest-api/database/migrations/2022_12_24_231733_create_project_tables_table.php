16fad9a6463a3f0938dedeb601d293de6c56857f
fahrizalm14 <fahrizalm14@gmail.com>
2022-12-25 13:19:59 +0700

    feat(api): add migration rest api app

---
+++ app/rest-api/database/migrations/2022_12_24_231733_create_project_tables_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_tables', function (Blueprint $table) {
+            $table->uuid("id")->primary();
+            $table->string("name");
+            $table->foreignUuid('project_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_tables');
+    }
+};
