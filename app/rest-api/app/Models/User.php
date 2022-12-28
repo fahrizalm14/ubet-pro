8c89ef651d1219e610c2b07ed5173bd3d6c8422e
fahrizalm14 <fahrizalm14@gmail.com>
2022-12-28 09:40:30 +0700

    refactor(api): update user model and join with 'project' table

--- app/rest-api/app/Models/User.php
+++ app/rest-api/app/Models/User.php
@@ -11,7 +11,14 @@ class User extends Model
     use Uuids;
     use HasFactory;
 
+    protected $hidden = ["created_at", "updated_at", "password"];
+
     protected $fillable = [
         "full_name", "username", "password"
     ];
+
+    public function project()
+    {
+        return $this->hasMany(Project::class);
+    }
 }
